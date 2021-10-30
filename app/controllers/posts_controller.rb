1aa1df760b3d49fada28ac23c8a3e467091d876b
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-30 15:50:28 -0700

    added carrierwave, added a post controller, updated post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+	
+	def new
+	end
+
+	def create
+	end
+	
+	def show
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:image)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	get "/dashboard" => "accounts#index"
 	
+	resources :posts, only: [:new, :create, :show]
+
 	root to: "public#homepage"
 end
